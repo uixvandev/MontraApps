ac922dcbb11b8e2d37c025bcfe7e19873cf7d171
irfan wahendra <2111527002_irfan@student.unand.ac.id>
2024-01-25 20:36:01 +0700

    Initial Commit

---
+++ MateriSesi4StateBinding/MateriSesi4StateBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MateriSesi4StateBindingApp.swift
+//  MateriSesi4StateBinding
+//
+//  Created by irfan wahendra on 25/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MateriSesi4StateBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
